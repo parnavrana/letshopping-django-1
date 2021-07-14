94dcce5bf2ead4a4157d3bc93d8da415ba115914
parnavrana <neeraj@srdvtechnologies.com>
2021-07-14 17:05:50 +0530

    store and carts functionality

--- letshopping/urls.py
+++ letshopping/urls.py
@@ -27,5 +27,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
     path('store/', include('store.urls')),
+    path('cart/', include('carts.urls')),
 
 ] + static(settings.MEDIA_URL, document_root= MEDIA_ROOT)
